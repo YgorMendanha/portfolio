5f5dd670741433c25a838b195e5bec9325281376
Ygor Mendanha <ygormendanha@gmail.com>
2025-07-29 16:44:33 -0400

    change sitemap

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,39 @@
+export async function GET() {
+  const siteUrl = "www.ygormendanha.com";
+
+  const paths = ["/"];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+  xsi:schemaLocation="
+    http://www.sitemaps.org/schemas/sitemap/0.9
+    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
+    http://www.w3.org/1999/xhtml
+    http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd">
+
+  ${paths
+    .map(
+      (path) => `
+    <url>
+      <loc>https://${siteUrl}${path}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>0.7</priority>
+      <xhtml:link rel="alternate" hreflang="pt" href="https://${siteUrl}/pt${path}" />
+      <xhtml:link rel="alternate" hreflang="en" href="https://${siteUrl}/en${path}" />
+
+    </url>`
+    )
+    .join("\n")}
+
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "stale-while-revalidate, s-maxage=3600",
+    },
+  });
+}
