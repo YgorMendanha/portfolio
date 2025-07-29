81563de5003422e464f40a261adf557b211f034b
Ygor Mendanha <ygormendanha@gmail.com>
2025-07-29 16:47:02 -0400

    change sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,8 +1,6 @@
 export async function GET() {
   const siteUrl = "www.ygormendanha.com";
 
-  const paths = ["/"];
-
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
